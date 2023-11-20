6ae068cec7e48962098e8797d57cd51cfc39b595
André Haswolinsk <haswolinsk12@gmail.com>
2023-11-20 00:04:44 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById( "scrollWin" ).onclick = () => {
+
+    document.getElementById( "scroll" ).scrollLeft += 30;
+
+}
+
+document.getElementById( "my-scroll-right-button" ).onclick = () => {
+
+    document.getElementById( "my-scroll-container" ).scrollLeft -= 30;
+
+}
